f6c62fe884da46f4de69ad2e7d96ccdfd428ea9b
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-03-09 02:46:53 +0700

    Ref. Controllers - Hasil Asesmen

--- application/controllers_progress/Hasilujian.php
+++ application/controllers_progress/Hasilujian.php
@@ -1,44 +1,43 @@
 <?php
 
 defined('BASEPATH') or exit('No direct script access allowed');
+
 class HasilUjian extends CI_Controller
 {
+    protected $user;
+
     public function __construct()
     {
         parent::__construct();
-        if ($this->ion_auth->logged_in()) {
-            $this->load->library(['datatables']);
-        } else {
+        if (!$this->ion_auth->logged_in()) {
             redirect('auth');
-            $this->load->library(['datatables']);
         }
+        $this->load->library(['datatables']);
         $this->load->model('Master_model', 'master');
         $this->load->model('Ujian_model', 'ujian');
         $this->user = $this->ion_auth->user()->row();
     }
+
     public function output_json($data, $encode = true)
     {
         if (!$encode) {
             $this->output->set_content_type('application/json')->set_output($data);
         } else {
-            $data = json_encode($data);
-            $this->output->set_content_type('application/json')->set_output($data);
+            $this->output->set_content_type('application/json')->set_output(json_encode($data));
         }
     }
+
     public function data()
     {
-        $nip_guru = null;
-        if (!$this->ion_auth->in_group('guru')) {
-            $this->output_json($this->ujian->getHasilUjian($nip_guru), false);
-        } else {
-            $nip_guru = $this->user->username;
-            $this->output_json($this->ujian->getHasilUjian($nip_guru), false);
-        }
+        $nip_guru = $this->ion_auth->in_group('guru') ? $this->user->username : null;
+        $this->output_json($this->ujian->getHasilUjian($nip_guru), false);
     }
+
     public function NilaiMhs($id)
     {
         $this->output_json($this->ujian->HslUjianById($id, true), false);
     }
+
     public function index()
     {
         $data = ['user' => $this->user, 'judul' => 'Ujian', 'subjudul' => 'Hasil Ujian'];
@@ -46,29 +45,34 @@ class HasilUjian extends CI_Controller
         $this->load->view('ujian/hasil');
         $this->load->view('_templates/dashboard/_footer.php');
     }
+
     public function detail($id)
     {
-        $ujian = $this->ujian->getUjianById($id);
-        $nilai = $this->ujian->bandingNilai($id);
-        $data = ['user' => $this->user, 'judul' => 'Ujian', 'subjudul' => 'Detail Hasil Ujian', 'ujian' => $ujian, 'nilai' => $nilai];
+        $data = [
+            'user'    => $this->user,
+            'judul'   => 'Ujian',
+            'subjudul' => 'Detail Hasil Ujian',
+            'ujian'   => $this->ujian->getUjianById($id),
+            'nilai'   => $this->ujian->bandingNilai($id),
+        ];
         $this->load->view('_templates/dashboard/_header.php', $data);
         $this->load->view('ujian/detail_hasil');
         $this->load->view('_templates/dashboard/_footer.php');
     }
+
     public function cetak($id)
     {
-        $mhs = $this->ujian->getIdMahasiswa($this->user->username);
+        $mhs   = $this->ujian->getIdMahasiswa($this->user->username);
         $hasil = $this->ujian->HslUjian($id, $mhs->id_siswa)->row();
-        $ujian = $this->ujian->getUjianById($id);
-        $data = ['ujian' => $ujian, 'hasil' => $hasil, 'mhs' => $mhs];
-        $this->load->view('ujian/cetak', $data);
+        $this->load->view('ujian/cetak', ['ujian' => $this->ujian->getUjianById($id), 'hasil' => $hasil, 'mhs' => $mhs]);
     }
+
     public function cetak_detail($id)
     {
-        $ujian = $this->ujian->getUjianById($id);
-        $nilai = $this->ujian->bandingNilai($id);
-        $hasil = $this->ujian->HslUjianById($id)->result();
-        $data = ['ujian' => $ujian, 'nilai' => $nilai, 'hasil' => $hasil];
-        $this->load->view('ujian/cetak_detail', $data);
+        $this->load->view('ujian/cetak_detail', [
+            'ujian'  => $this->ujian->getUjianById($id),
+            'nilai'  => $this->ujian->bandingNilai($id),
+            'hasil'  => $this->ujian->HslUjianById($id)->result(),
+        ]);
     }
 }
