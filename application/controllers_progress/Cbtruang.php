925c12aac626d1857e238f32be30d8fc04d71cd7
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-03-09 02:16:22 +0700

    Ref. Controllers - Asesmen Ruang

--- application/controllers_progress/Cbtruang.php
+++ application/controllers_progress/Cbtruang.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('BASEPATH') or exit('No direct script access allowed');
+
 class Cbtruang extends CI_Controller
 {
     public function __construct()
@@ -7,9 +9,8 @@ class Cbtruang extends CI_Controller
         parent::__construct();
         if (!$this->ion_auth->logged_in()) {
             redirect('auth');
-        } else {
-            if ($this->ion_auth->is_admin()) {
-            }
+        }
+        if (!$this->ion_auth->is_admin()) {
             show_error('Hanya Administrator yang diberi hak untuk mengakses halaman ini, <a href="' . base_url('dashboard') . '">Kembali ke menu awal</a>', 403, 'Akses Terlarang');
         }
         $this->load->library(['datatables', 'form_validation']);
@@ -18,52 +19,64 @@ class Cbtruang extends CI_Controller
         $this->load->model('Cbt_model', 'cbt');
         $this->form_validation->set_error_delimiters('', '');
     }
+
     public function output_json($data, $encode = true)
     {
         if (!$encode) {
             $this->output->set_content_type('application/json')->set_output($data);
         } else {
-            $data = json_encode($data);
-            $this->output->set_content_type('application/json')->set_output($data);
+            $this->output->set_content_type('application/json')->set_output(json_encode($data));
         }
     }
+
     public function index()
     {
         $user = $this->ion_auth->user()->row();
-        $data = ['user' => $user, 'judul' => 'Ruang Ujian', 'subjudul' => 'Data Ruang Ujian', 'profile' => $this->dashboard->getProfileAdmin($user->id), 'setting' => $this->dashboard->getSetting()];
-        $data['tp'] = $this->dashboard->getTahun();
-        $data['tp_active'] = $this->dashboard->getTahunActive();
-        $data['smt'] = $this->dashboard->getSemester();
-        $data['smt_active'] = $this->dashboard->getSemesterActive();
+        $data = [
+            'user'      => $user,
+            'judul'     => 'Ruang Ujian',
+            'subjudul'  => 'Data Ruang Ujian',
+            'profile'   => $this->dashboard->getProfileAdmin($user->id),
+            'setting'   => $this->dashboard->getSetting(),
+            'tp'        => $this->dashboard->getTahun(),
+            'tp_active' => $this->dashboard->getTahunActive(),
+            'smt'       => $this->dashboard->getSemester(),
+            'smt_active' => $this->dashboard->getSemesterActive(),
+        ];
         $this->load->view('_templates/dashboard/_header', $data);
         $this->load->view('cbt/ruang/data');
         $this->load->view('_templates/dashboard/_footer');
     }
+
     public function data()
     {
         $this->output_json($this->cbt->getRuang(), false);
     }
+
     public function add()
     {
-        $insert = ['nama_ruang' => $this->input->post('nama_ruang', true), 'kode_ruang' => $this->input->post('kode_ruang', true)];
+        $insert = [
+            'nama_ruang' => $this->input->post('nama_ruang', true),
+            'kode_ruang' => $this->input->post('kode_ruang', true),
+        ];
         $this->master->create('cbt_ruang', $insert, false);
-        $data['status'] = $insert;
-        $this->output_json($data);
+        $this->output_json(['status' => $insert]);
     }
+
     public function update()
     {
         $data = $this->cbt->updateRuang();
         $this->output->set_content_type('application/json')->set_output($data);
     }
+
     public function delete()
     {
         $chk = $this->input->post('checked', true);
         if (!$chk) {
             $this->output_json(['status' => false]);
-        } else {
-            if (!$this->master->delete('cbt_ruang', $chk, 'id_ruang')) {
-            }
-            $this->output_json(['status' => true, 'total' => count($chk)]);
+            return;
         }
+        $this->master->delete('cbt_ruang', $chk, 'id_ruang');
+        $this->output_json(['status' => true, 'total' => count($chk)]);
     }
 }
