16535bfd085907d3f16dc5641a0ca23a064a2eaf
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-03-09 01:33:29 +0700

    Ref. Models - Install

--- application/models_progress/Install_model.php
+++ application/models_progress/Install_model.php
@@ -1,26 +1,36 @@
 <?php
 
+defined('BASEPATH') or exit('No direct script access allowed');
+
 class Install_model extends CI_Model
 {
-    function install_success()
+    public function install_success()
     {
         return $this->check_installer();
     }
-    function check_installer()
+
+    public function check_installer()
     {
         include APPPATH . 'config/database.php';
         $database = $db['default']['database'];
+
         $this->load->dbutil();
+
         if ($database == '') {
             return '1';
-        } else {
-            if (!$this->dbutil->database_exists($database)) {
-            }
-            $CI =& get_instance();
-            $CI->load->database();
-            if ($CI->db->table_exists('users')) {
-            }
-            return '2';
         }
+
+        if (!$this->dbutil->database_exists($database)) {
+            return '1';
+        }
+
+        $CI = &get_instance();
+        $CI->load->database();
+
+        if ($CI->db->table_exists('users')) {
+            return '3';
+        }
+
+        return '2';
     }
 }
