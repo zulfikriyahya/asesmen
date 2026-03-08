2690fc31250d31f76c67bdb27b77f133b3f8221a
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-03-09 03:29:59 +0700

    Ref. Controllers - Wali Struktur

--- application/controllers_progress/Walistruktur.php
+++ application/controllers_progress/Walistruktur.php
@@ -1,17 +1,25 @@
 <?php
 
+defined('BASEPATH') or exit('No direct script access allowed');
+
 class Walistruktur extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
+
         if (!$this->ion_auth->logged_in()) {
             redirect('auth');
-        } else {
-            if (!(!$this->ion_auth->is_admin() && !$this->ion_auth->in_group('guru'))) {
-            }
-            show_error('Hanya Administrator dan guru yang diberi hak untuk mengakses halaman ini, <a href="' . base_url('dashboard') . '">Kembali ke menu awal</a>', 403, 'Akses Terlarang');
         }
+
+        if (!$this->ion_auth->is_admin() && !$this->ion_auth->in_group('guru')) {
+            show_error(
+                'Hanya Administrator dan guru yang diberi hak untuk mengakses halaman ini, <a href="' . base_url('dashboard') . '">Kembali ke menu awal</a>',
+                403,
+                'Akses Terlarang'
+            );
+        }
+
         $this->load->library(['datatables', 'form_validation']);
         $this->load->model('Master_model', 'master');
         $this->load->model('Dashboard_model', 'dashboard');
@@ -19,48 +27,76 @@ class Walistruktur extends CI_Controller
         $this->load->model('Dropdown_model', 'dropdown');
         $this->form_validation->set_error_delimiters('', '');
     }
+
     public function output_json($data, $encode = true)
     {
         if (!$encode) {
             $this->output->set_content_type('application/json')->set_output($data);
         } else {
-            $data = json_encode($data);
-            $this->output->set_content_type('application/json')->set_output($data);
+            $this->output->set_content_type('application/json')->set_output(json_encode($data));
         }
     }
+
     public function index()
     {
-        $user = $this->ion_auth->user()->row();
-        $data = ['user' => $user, 'judul' => 'Struktur Organisasi', 'subjudul' => 'Struktur Organisasi', 'setting' => $this->dashboard->getSetting()];
-        $tp = $this->master->getTahunActive();
-        $smt = $this->master->getSemesterActive();
-        $data['tp'] = $this->dashboard->getTahun();
-        $data['tp_active'] = $tp;
-        $data['smt'] = $this->dashboard->getSemester();
-        $data['smt_active'] = $smt;
-        $guru = $this->dashboard->getDataGuruByUserId($user->id, $tp->id_tp, $smt->id_smt);
-        $struktur = $this->kelas->getStrukturKelas($guru->wali_kelas);
-        if ($struktur == null) {
-            $data['struktur'] = json_decode(json_encode($this->kelas->dummyStruktur()));
-        } else {
-            $data['struktur'] = $struktur;
+        $user  = $this->ion_auth->user()->row();
+        $tp    = $this->master->getTahunActive();
+        $smt   = $this->master->getSemesterActive();
+        $guru  = $this->dashboard->getDataGuruByUserId($user->id, $tp->id_tp, $smt->id_smt);
+
+        $struktur_raw = $this->kelas->getStrukturKelas($guru->wali_kelas);
+        $struktur     = $struktur_raw ?? json_decode(json_encode($this->kelas->dummyStruktur()));
+
+        $siswa   = $this->kelas->getKelasSiswa($guru->wali_kelas, $tp->id_tp, $smt->id_smt);
+        $siswas  = ['' => 'Pilih Siswa'];
+        foreach ($siswa as $s) {
+            $siswas[$s->id_siswa] = $s->nama;
         }
-        $data['guru'] = $guru;
-        $data['gurus'] = $this->dropdown->getAllGuru();
-        $siswa = $this->kelas->getKelasSiswa($guru->wali_kelas, $tp->id_tp, $smt->id_smt);
-        $siswas[''] = 'Pilih Siswa';
-        foreach ($siswa as $key => $value) {
-            $siswas[$value->id_siswa] = $value->nama;
-        }
-        $data['siswas'] = $siswas;
-        $data['id_kelas'] = $guru->wali_kelas;
+
+        $data = [
+            'user'      => $user,
+            'judul'     => 'Struktur Organisasi',
+            'subjudul'  => 'Struktur Organisasi',
+            'setting'   => $this->dashboard->getSetting(),
+            'tp'        => $this->dashboard->getTahun(),
+            'tp_active' => $tp,
+            'smt'       => $this->dashboard->getSemester(),
+            'smt_active' => $smt,
+            'guru'      => $guru,
+            'gurus'     => $this->dropdown->getAllGuru(),
+            'struktur'  => $struktur,
+            'siswas'    => $siswas,
+            'id_kelas'  => $guru->wali_kelas,
+        ];
+
         $this->load->view('members/guru/templates/header', $data);
         $this->load->view('members/guru/wali/struktur');
         $this->load->view('members/guru/templates/footer');
     }
+
     public function save()
     {
-        $data = ['id_kelas' => $this->input->post('id_kelas'), 'ketua' => $this->input->post('ketua'), 'wakil_ketua' => $this->input->post('wakil_ketua'), 'sekretaris_1' => $this->input->post('sekretaris_1'), 'sekretaris_2' => $this->input->post('sekretaris_2'), 'bendahara_1' => $this->input->post('bendahara_1'), 'bendahara_2' => $this->input->post('bendahara_2'), 'sie_ekstrakurikuler' => $this->input->post('sie_ekstrakurikuler'), 'sie_upacara' => $this->input->post('sie_upacara'), 'sie_olahraga' => $this->input->post('sie_olahraga'), 'sie_keagamaan' => $this->input->post('sie_keagamaan'), 'sie_keamanan' => $this->input->post('sie_keamanan'), 'sie_ketertiban' => $this->input->post('sie_ketertiban'), 'sie_kebersihan' => $this->input->post('sie_kebersihan'), 'sie_keindahan' => $this->input->post('sie_keindahan'), 'sie_kesehatan' => $this->input->post('sie_kesehatan'), 'sie_kekeluargaan' => $this->input->post('sie_kekeluargaan'), 'sie_humas' => $this->input->post('sie_humas')];
+        $data = [
+            'id_kelas'            => $this->input->post('id_kelas', true),
+            'ketua'               => $this->input->post('ketua', true),
+            'wakil_ketua'         => $this->input->post('wakil_ketua', true),
+            'sekretaris_1'        => $this->input->post('sekretaris_1', true),
+            'sekretaris_2'        => $this->input->post('sekretaris_2', true),
+            'bendahara_1'         => $this->input->post('bendahara_1', true),
+            'bendahara_2'         => $this->input->post('bendahara_2', true),
+            'sie_ekstrakurikuler' => $this->input->post('sie_ekstrakurikuler', true),
+            'sie_upacara'         => $this->input->post('sie_upacara', true),
+            'sie_olahraga'        => $this->input->post('sie_olahraga', true),
+            'sie_keagamaan'       => $this->input->post('sie_keagamaan', true),
+            'sie_keamanan'        => $this->input->post('sie_keamanan', true),
+            'sie_ketertiban'      => $this->input->post('sie_ketertiban', true),
+            'sie_kebersihan'      => $this->input->post('sie_kebersihan', true),
+            'sie_keindahan'       => $this->input->post('sie_keindahan', true),
+            'sie_kesehatan'       => $this->input->post('sie_kesehatan', true),
+            'sie_kekeluargaan'    => $this->input->post('sie_kekeluargaan', true),
+            'sie_humas'           => $this->input->post('sie_humas', true),
+        ];
+
         $insert = $this->db->replace('kelas_struktur', $data);
         $this->output_json($insert);
     }
